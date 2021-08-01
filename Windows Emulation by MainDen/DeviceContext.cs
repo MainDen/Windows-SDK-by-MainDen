ed4c0889408ff8be9231b3b4e95d0f2c6c1a65b0
MainDen <50581460+MainDen@users.noreply.github.com>
2021-08-01 16:07:42 +0300

    Add new methods.

--- Windows Emulation by MainDen/DeviceContext.cs
+++ Windows Emulation by MainDen/DeviceContext.cs
@@ -14,16 +14,62 @@ namespace MainDen.Windows.Emulation
 
         public IntPtr CaptureWindow(int cropLeft, int cropTop, int cropRight, int cropBottom)
         {
-            IntPtr hSource = Window.GetWindowDC(windowHandle);
             Window.GetWindowRect(windowHandle, out Window.RECT windowRect);
+            int x = cropLeft;
+            int y = cropTop;
             int width = Math.Max(0, windowRect.Width - (cropLeft + cropRight));
             int height = Math.Max(0, windowRect.Height - (cropTop + cropBottom));
-            IntPtr hDest = GDI.CreateCompatibleDC(hSource);
+            return Capture(windowHandle, x, y, width, height);
+        }
+
+        public IntPtr CaptureScreen()
+        {
+            return CaptureScreen(0, 0, 0, 0);
+        }
+
+        public IntPtr CaptureScreen(int cropLeft, int cropTop, int cropRight, int cropBottom)
+        {
+            Window.GetWindowRect(windowHandle, out Window.RECT windowRect);
+            int x = windowRect.Left + cropLeft;
+            int y = windowRect.Top + cropTop;
+            int width = Math.Max(0, windowRect.Width - (cropLeft + cropRight));
+            int height = Math.Max(0, windowRect.Height - (cropTop + cropBottom));
+            return Capture(IntPtr.Zero, x, y, width, height);
+        }
+
+        public static IntPtr CaptureDesktop()
+        {
+            return CaptureDesktop(0, 0, 0, 0);
+        }
+
+        public static IntPtr CaptureDesktop(int cropLeft, int cropTop, int cropRight, int cropBottom)
+        {
+            IntPtr hDesktop = Window.GetWindowDC(IntPtr.Zero);
+            int desktopWidth = GDI.GetDeviceCaps(hDesktop, GDI.DeviceCaps.HORZRES);
+            int desktopHeight = GDI.GetDeviceCaps(hDesktop, GDI.DeviceCaps.VERTRES);
+            Window.ReleaseDC(IntPtr.Zero, hDesktop);
+            int x = cropLeft;
+            int y = cropTop;
+            int width = Math.Max(0, desktopWidth - (cropLeft + cropRight));
+            int height = Math.Max(0, desktopHeight - (cropTop + cropBottom));
+            return Capture(IntPtr.Zero, x, y, width, height);
+        }
+
+        public static IntPtr Capture(IntPtr windowHandle, int x, int y, int width, int height)
+        {
+            if (width < 0)
+                throw new ArgumentOutOfRangeException(nameof(width));
+            if (height < 0)
+                throw new ArgumentOutOfRangeException(nameof(height));
+            if (width == 0 || height == 0)
+                return IntPtr.Zero;
+            IntPtr hSource = Window.GetWindowDC(windowHandle);
+            IntPtr hMemory = GDI.CreateCompatibleDC(hSource);
             IntPtr hBitmap = GDI.CreateCompatibleBitmap(hSource, width, height);
-            IntPtr hOld = GDI.SelectObject(hDest, hBitmap);
-            GDI.BitBlt(hDest, 0, 0, width, height, hSource, cropLeft, cropTop, GDI.SRCCOPY);
-            GDI.SelectObject(hDest, hOld);
-            GDI.DeleteDC(hDest);
+            IntPtr hPrimaryBitmap = GDI.SelectObject(hMemory, hBitmap);
+            GDI.BitBlt(hMemory, 0, 0, width, height, hSource, x, y, GDI.RasterOperationCodes.SRCCOPY);
+            GDI.SelectObject(hMemory, hPrimaryBitmap);
+            GDI.DeleteDC(hMemory);
             Window.ReleaseDC(windowHandle, hSource);
             return hBitmap;
         }

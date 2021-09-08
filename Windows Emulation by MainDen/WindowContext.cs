469e8ed45444d49f970724b859f2188d5d050d6d
MainDen <50581460+MainDen@users.noreply.github.com>
2021-09-08 05:02:05 +0300

    Add SetWindowPos inherited methods.

--- Windows Emulation by MainDen/WindowContext.cs
+++ Windows Emulation by MainDen/WindowContext.cs
@@ -30,5 +30,41 @@ namespace MainDen.Windows.Emulation
         {
             return Window.GetClientRect(WindowHandle, out rect);
         }
+
+        public bool Move(int x, int y)
+        {
+            var flags = Window.SetWindowPosFlags.NoSize | Window.SetWindowPosFlags.NoZOrder;
+            return Window.SetWindowPos(WindowHandle, IntPtr.Zero, x, y, 0, 0, flags);
+        }
+
+        public bool Resize(int width, int height)
+        {
+            var flags = Window.SetWindowPosFlags.NoMove | Window.SetWindowPosFlags.NoZOrder;
+            return Window.SetWindowPos(WindowHandle, IntPtr.Zero, 0, 0, width, height, flags);
+        }
+
+        public bool TopMost
+        {
+            set
+            {
+                var flags = Window.SetWindowPosFlags.NoMove | Window.SetWindowPosFlags.NoSize;
+                if (value)
+                    Window.SetWindowPos(WindowHandle, Window.InsertAfter.TopMost, 0, 0, 0, 0, flags);
+                else
+                    Window.SetWindowPos(WindowHandle, Window.InsertAfter.NoTopMost, 0, 0, 0, 0, flags);
+            }
+        }
+
+        public bool Visible
+        {
+            set
+            {
+                var flags = Window.SetWindowPosFlags.NoMove | Window.SetWindowPosFlags.NoSize | Window.SetWindowPosFlags.NoZOrder;
+                if (value)
+                    Window.SetWindowPos(WindowHandle, IntPtr.Zero, 0, 0, 0, 0, flags | Window.SetWindowPosFlags.ShowWindow);
+                else
+                    Window.SetWindowPos(WindowHandle, IntPtr.Zero, 0, 0, 0, 0, flags | Window.SetWindowPosFlags.HideWindow);
+            }
+        }
     }
 }
